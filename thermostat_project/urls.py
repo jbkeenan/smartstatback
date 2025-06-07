a0cfb65a10fb3cdab8cf736107013db997c02f02
GitHub Action <action@github.com>
2025-06-07 19:03:39 +0000

    Update backend from ZIP deployment

---
+++ thermostat_project/urls.py
@@ -0,0 +1,24 @@
+"""
+URL Configuration for thermostat_project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+# API root router
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/', include('authentication.urls')),
+    path('api/thermostats/', include('thermostats.urls')),
+    path('api/properties/', include('properties.urls')),
+]
